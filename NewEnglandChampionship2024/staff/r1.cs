461f9db5f473dd52b2f59df44785f876b04f574e
Tim Reynolds <tim@cubingusa.org>
2024-08-12 08:01:47 -0400

    Tweak staff counts.

--- NewEnglandChampionship2024/staff/r1.cs
+++ NewEnglandChampionship2024/staff/r1.cs
@@ -1,6 +1,6 @@
 #include "_lib.cs"
 
-RoundOneStaffAssignments(_777-r1, [], [Judges(8), Scramblers(2), Runners(1), Delegates(1)], 2)
+RoundOneStaffAssignments(_777-r1, [], [Judges(8), Scramblers(3), Runners(1), Delegates(1)], 2)
 RoundOneStaffAssignments(_minx-r1, Persons(CompetingIn(_777)),
                          [Judges(12), Scramblers(3), Runners(2), Delegates(1)], 3)
 RoundOneStaffAssignments(_666-r1,
@@ -21,7 +21,7 @@ RoundOneStaffAssignments(_333bf-r1,
                          [Judges(10), Scramblers(2), Runners(2), Delegates(1)], 2)
 
 RoundOneStaffAssignments(_333-r1, [],
-                         [Judges(14), Scramblers(3), Runners(3), Delegates(1)], 5)
+                         [Judges(14), Scramblers(3), Runners(4), Delegates(1)], 5)
 RoundOneStaffAssignments(_444-r1, Persons(CompetingIn(_333)),
                          [Judges(14), Scramblers(3), Runners(3), Delegates(1)], 3)
 RoundOneStaffAssignments(_pyram-r1, Persons(CompetingIn(_555)),
@@ -31,4 +31,8 @@ RoundOneStaffAssignments(_555-r1,
                                     CompetingIn(_skewb))),
                          [Judges(12), Scramblers(4), Runners(2), Delegates(1)], 2)
 RoundOneStaffAssignments(_skewb-r1, Persons(CompetingIn(_555)),
+                         [Judges(14), Scramblers(3), Runners(4), Delegates(1)], 3)
+RoundOneStaffAssignments(_222-r1, [],
+                         [Judges(14), Scramblers(3), Runners(4), Delegates(1)], 4)
+RoundOneStaffAssignments(_333oh-r1, [],
                          [Judges(14), Scramblers(3), Runners(3), Delegates(1)], 3)
