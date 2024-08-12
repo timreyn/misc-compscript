acabd312f829fabf3f9d9d3caeda0f838f99c56b
Tim Reynolds <tim@cubingusa.org>
2024-08-12 08:01:02 -0400

    Add scorer and top competitors.

--- NewEnglandChampionship2024/groups/_lib.cs
+++ NewEnglandChampionship2024/groups/_lib.cs
@@ -2,10 +2,12 @@
 #include "../lib/_staff_teams.cs"
 
 Define("RoundOneAssignmentSets",
-       [AssignmentSet("Delegates", IsDelegate(Arg<Person>()), true),
+       [AssignmentSet("Top", (PsychSheetPosition({1, Event}) < {2, Number}), (Stage() == RED), featured=true),
+        AssignmentSet("Delegates", IsDelegate(Arg<Person>()), true),
         AssignmentSet("Data", (StaffTeam(Arg<Person>()) == DATA), (Stage() == RED)),
         AssignmentSet("Everyone", true, true)])
 
 Define("GroupScorers",
-       Map(Stages(), ByFilters((StaffTeam(Arg<Person>()) == Arg<String>()),
-                               (Stage() == Arg<String>()), 10)))
+       Concat(Map(Stages(), ByFilters((StaffTeam(Arg<Person>()) == Arg<String>()),
+                                      (Stage() == Arg<String>()), 10)),
+              [ByMatchingValue(FirstName(), -5)]))
--- NewEnglandChampionship2024/groups/r1.cs
+++ NewEnglandChampionship2024/groups/r1.cs
@@ -3,4 +3,7 @@
 Map([_777-r1, _minx-r1, _666-r1, _clock-r1, _sq1-r1, _333bf-r1,
      _333-r1, _444-r1, _pyram-r1, _555-r1, _skewb-r1,
      _222-r1, _333oh-r1],
-    AssignGroups(Arg<Round>(), RoundOneAssignmentSets(), GroupScorers()))
+    AssignGroups(Arg<Round>(),
+                 RoundOneAssignmentSets(EventForRound(Arg<Round>()),
+                                        (Length(Groups(Arg<Round>())) * 0.7)),
+                 GroupScorers()))
